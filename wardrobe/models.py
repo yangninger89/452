4e0fc3d5eb4fe2e2a55f0eedfbedb57138f8ffe7
Nan Yang <yangnan89@gmail.com>
2017-11-29 20:18:44 -0600

    make page pretty using CSS and bootstrap

--- wardrobe/models.py
+++ wardrobe/models.py
@@ -7,12 +7,12 @@ class Post(models.Model):
     owner = models.ForeignKey('auth.User')
     type = models.CharField(max_length=200)
     description = models.TextField()
-    created_data = models.DateTimeField(default=timezone.now)
-    published_data = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
 
     def publish(self):
-        self.published_data = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
 
